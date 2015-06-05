9bdfdee621bcacc2052a22edf5965241a0b3d551
akamediasystem <nfeehan@gmail.com>
2015-06-05 15:34:14 -0400

    finalizing barnacle functions

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@ cp chalkboard.service chalkboard-resonator.service /etc/systemd/system
 cp avahi-resonator.service /etc/avahi/services/
 systemctl enable chalkboard.service chalkboard-resonator.service
 systemctl start chalkboard.service chalkboard-resonator.service
+systemctl | grep AKA
