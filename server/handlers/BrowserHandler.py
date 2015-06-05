f263e0a42390c6579b296ac29acf927cfb587ebf
akamediasystem <nfeehan@gmail.com>
2015-06-05 12:07:00 -0400

    bones are working :lunch:

--- server/handlers/BrowserHandler.py
+++ server/handlers/BrowserHandler.py
@@ -18,7 +18,7 @@ class BrowserHandler(BaseHandler):
         k = db.lrange('msgs', 0, MAX_MSGS)
         if k is None:
             k = ['no messages right now']
-        self.write(repr(self.request))
+        k.append(self.request.remote_ip)
         self.write(loader.load("main.html").generate(keywords=k))
         self.finish()
 
