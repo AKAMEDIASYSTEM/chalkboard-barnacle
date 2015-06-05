876c03f90f24c103a3d92ac99efd056f67d1203d
akamediasystem <nfeehan@gmail.com>
2015-06-05 11:23:47 -0400

    oh yeah

--- server/handlers/BrowserHandler.py
+++ server/handlers/BrowserHandler.py
@@ -7,18 +7,15 @@ from handlers.BaseHandler import BaseHandler
 
 
 class BrowserHandler(BaseHandler):
-    '''HTML display of Keywords browsed in the last day'''
-
-    MAX_MSGS = 10
+    '''HTML display of the chalkboard'''
 
     def get(self):
+        MAX_MSGS = 10
         loader = tornado.template.Loader('../server/templates')
         # n = self.get_argument('n', 3)
         db = self.settings['db']
         logging.debug('hit the BrowserHandler endpoint')
-        keywords = []
-        found = 0
-        k = db.lrange('msgs',0,MAX_MSGS)
+        k = db.lrange('msgs', 0, MAX_MSGS)
         if k is None:
             k = ['no messages right now']
         self.write(loader.load("main.html").generate(keywords=k))
