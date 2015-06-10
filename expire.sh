147ac8d5b664de2cb56dc7e5d6b8f5af8b19e406
akamediasystem <nfeehan@gmail.com>
2015-06-10 12:30:26 -0400

    binbash stuff confusion

--- expire.sh
+++ expire.sh
@@ -1,1 +1,2 @@
+#!/bin/ bash
 curl -d "utterance= . . . " localhost
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+#!/bin/ bash
 echo 'installing chalkboard-barnacle now'
 chmod +x expire.sh
 cp chalkboard.service chalkboard-expire.service chalkboard-resonator.service /etc/systemd/system
